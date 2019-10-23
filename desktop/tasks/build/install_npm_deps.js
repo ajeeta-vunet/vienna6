15e349a6ae686c1f2b61e159ac43f36959278a02
Jyothi Lakshmi <jyothi@vunet.local>
2019-10-23 17:35:28 +0530

    ER 1674:Add Vega visualization in vienna6

--- desktop/tasks/build/install_npm_deps.js
+++ desktop/tasks/build/install_npm_deps.js
@@ -7,6 +7,15 @@ module.exports = function (grunt) {
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });
+
+  // This is to run npm install inside vega_vis folder.
+  grunt.registerTask('_build:installNpmDepsForVega', function () {
+    grunt.file.mkdir('build/kibana/plugins/vega_vis/node_modules');
+
+    exec('npm install  --production --no-optional', {
+      cwd: grunt.config.process('<%= root %>/build/kibana')
+    }, this.async());
+  });
 };
 
 

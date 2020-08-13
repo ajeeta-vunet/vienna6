d9d1b7621aa8d7194f3556b2f9cfbee893481378
shubham@vunet.local <shubham@vunet.local>
2020-08-13 14:12:30 +0530

    ER 2003 : BMV naming convention in Mobile View

--- mobile/libs/utils/src/lib/title-case.ts
+++ mobile/libs/utils/src/lib/title-case.ts
@@ -1,8 +1,9 @@
 export function toTitleCase(str: string) {
-    return str.replace(/[-_]/giu,' ').replace(
-        /\w\S*/giu,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
+    return str.replace(/[-_]/giu,' ')
+    // .replace(
+    //     /\w\S*/giu,
+    //     function(txt) {
+    //         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    //     }
+    // );
 }

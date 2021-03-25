cd2eefbe3fd76d8636d2d31be69a5f09a5314048
ramya-vunet <77725894+ramya-vunet@users.noreply.github.com>
2021-03-25 20:15:42 +0530

    ER 2226 : Issues in automatic update of interlinked visualization/saved search permissions based on dashboard user permissions (#143)
    * Solving issues of ER-2198
    
    * Dashabord permisisons update along with inter linked visualizations saved searches is done
    
    * ER-2226 : removedduplicate update
    
    * ER-2226 : Added all comments made code proper
    
    * ER-2226 : When directly linked vizs/savedsearhces having same user role permissions we need to check if inter linked vizs saved searches are having lower permissions or not if they are having lower permissions we need to show popup in that case also
    
    * ER-2226 : resolved all the comments given by for this ER
    
    * ER-2226 : resolved all typo related comments given by for this ER

---
+++ desktop/src/core_plugins/kibana/public/dashboard/lib/get_visualizations_interlinked_data.js
@@ -0,0 +1,155 @@
+import { updateVisualizationPermissions } from './update_visualization_permissions';
+
+export async function getVisualizationInterlinkedData(
+  updatedVisualizations,
+  dashboardPermissions,
+  savedVisualizations,
+  visualizationIds,
+  savedSearchIds) {
+
+  //This is to store inter linked saved seaches id's
+  const interlinkedSavedSearcheIds = [];
+
+  //This is to store inter linked BMV's id's
+  const interlinkedBMVIds = [];
+
+  //This is an object having interlinked saved searches list and interlinked bmvs
+  const allInterlinkedIdsOfDashboard = {};
+
+  //This is to store interlinkedVisualizationId's without duplicates
+  const bmvIdsWithoutDuplicates = [];
+
+  //This is to store interlinkedSavedSearcheId's without duplicates
+  const savedSearchesIdsWithoutDuplicates = [];
+
+  updatedVisualizations.map((eachVisualization) => {
+    //if saved searchId is undefined that means the visulization is not created directly using saved search
+    if (eachVisualization.savedSearchId === undefined) {
+      if (eachVisualization.vis.type.name === 'business_metric') {
+        //when dashbaord is created using Business metric visulization
+        const bmvMetrics = eachVisualization.vis.params.metrics;
+        bmvMetrics.map((eachMetric) => {
+          if (eachMetric.savedSearch && eachMetric.savedSearch.id !== '') {
+            interlinkedSavedSearcheIds.push(eachMetric.savedSearch.id);
+          }
+        });
+      } else if(eachVisualization.vis.type.name === 'status_indicator_and_kpi') {
+        //when dashbaord is created using KPI visualization
+        const kpiParameters = eachVisualization.vis.params.parameters;
+        kpiParameters.map((eachParameter) => {
+          if (eachParameter.name && !interlinkedBMVIds.includes(eachParameter.name.id)) {
+            interlinkedBMVIds.push(eachParameter.name.id);
+          }
+        });
+      } else if(eachVisualization.vis.type.name === 'bullet_vis') {
+        //when dashbaord is created using Bullet visualization
+        const bullets = eachVisualization.vis.params.bullets;
+        bullets.map((eachBullet) => {
+          if (eachBullet.name && !interlinkedBMVIds.includes(eachBullet.name.id)) {
+            interlinkedBMVIds.push(eachBullet.name.id);
+          }
+        });
+      } else if(eachVisualization.vis.type.name === 'insight_vis') {
+        //when dashbaord is created using Insight visulization
+        const insightBMVs = eachVisualization.vis.params.bmv;
+        insightBMVs.map((eachBMV) => {
+          if (eachBMV.name  && !interlinkedBMVIds.includes(eachBMV.name.id)) {
+            interlinkedBMVIds.push(eachBMV.name.id);
+          }
+        });
+      } else if(eachVisualization.vis.type.name === 'customer_journey_map') {
+        //when dashbaord is created using Customer Journer Map visualization
+        const metricGroups = eachVisualization.vis.params.metricGroups;
+        const stages = eachVisualization.vis.params.stages;
+        stages.map((eachStage) => {
+          const stageName = eachStage.name;
+          metricGroups.map((eachBMV) => {
+            if (eachBMV.metrics && !interlinkedBMVIds.includes(eachBMV.metrics[stageName].id)) {
+              const eachStageBMV = eachBMV.metrics[stageName].id;
+              interlinkedBMVIds.push(eachStageBMV);
+            }
+          });
+        });
+      } else if(eachVisualization.vis.type.name === 'unified_transaction_map') {
+        //when dashbaord is created using Unified transaction map visualization
+        const graphs = eachVisualization.vis.params.graphs;
+        const links = eachVisualization.vis.params.customLinks;
+        const nodes = eachVisualization.vis.params.customNodes;
+
+        //Getting BMV's added for graphs,subgraphs and metrics
+        graphs.map((eachGraph) => {
+          if (!interlinkedBMVIds.includes(eachGraph.bmv.id)) {
+            interlinkedBMVIds.push(eachGraph.bmv.id);
+          }
+          eachGraph.subGraphVisList.map((eachSubgraph) => {
+            if (!interlinkedBMVIds.includes(eachSubgraph.id)) {
+              interlinkedBMVIds.push(eachSubgraph.id);
+            }
+          });
+          eachGraph.metrics.map((eachMetric) => {
+            if (!interlinkedBMVIds.includes(eachMetric.name.id)) {
+              interlinkedBMVIds.push(eachMetric.name.id);
+            }
+          });
+        });
+        //Getting BMV's added for links
+        links.map((eachLink) => {
+          eachLink.metrics.map((eachMetric) => {
+            if (!interlinkedBMVIds.includes(eachMetric.name.id)) {
+              interlinkedBMVIds.push(eachMetric.name.id);
+            }
+          });
+        });
+        //Getting BMV's added for nodes
+        nodes.map((eachNode) => {
+          eachNode.metrics.map((eachMetric) => {
+            if (!interlinkedBMVIds.includes(eachMetric.name.id)) {
+              interlinkedBMVIds.push(eachMetric.name.id);
+            }
+          });
+        });
+      }
+    } else {
+      //If any visualizations are created using saved searches getting those inter linked id's
+      if (!interlinkedSavedSearcheIds.includes(eachVisualization.savedSearchId)) {
+        interlinkedSavedSearcheIds.push(eachVisualization.savedSearchId);
+      }
+    }
+  });
+
+  //Getting interlinked BMV visualizations updated object
+  const BMVlinkedSavedSearches = Promise.resolve(
+    updateVisualizationPermissions(dashboardPermissions, interlinkedBMVIds, savedVisualizations)
+  );
+  //Getting BMV's inter linked saved searches
+  await BMVlinkedSavedSearches.then(function (result) {
+    result.visualizations.map((eachBMVviz) => {
+      const bmvMetrics = eachBMVviz.vis.params.metrics;
+      bmvMetrics.map((eachMetric) => {
+        if (eachMetric.savedSearch && eachMetric.savedSearch.id !== '' && !interlinkedSavedSearcheIds.includes(eachMetric.savedSearch.id)) {
+          interlinkedSavedSearcheIds.push(eachMetric.savedSearch.id);
+        }
+      });
+    });
+  });
+
+  //To remove duplicate visualization id's
+  //we already checking for duplictes in above code but still we need to check with directly linked visulizations so here we are cheking one more time
+  interlinkedBMVIds.map((eachVizId) => {
+    if (!visualizationIds.includes(eachVizId) && !bmvIdsWithoutDuplicates.includes(eachVizId)) {
+      bmvIdsWithoutDuplicates.push(eachVizId);
+    }
+  });
+  //To remove duplicate savedsearch id's
+  //we may have directly linked saved searches which may be also inter linked to any BMV and for multiple BMV's same saved search may be linked so here we are removing duplicates
+  interlinkedSavedSearcheIds.map((eachSavedSearchId) => {
+    if (!savedSearchIds.includes(eachSavedSearchId) && !savedSearchesIdsWithoutDuplicates.includes(eachSavedSearchId)) {
+      savedSearchesIdsWithoutDuplicates.push(eachSavedSearchId);
+    }
+  });
+
+  allInterlinkedIdsOfDashboard.linkedBmvIds = bmvIdsWithoutDuplicates;
+  allInterlinkedIdsOfDashboard.linkedSavedSearcheIds = savedSearchesIdsWithoutDuplicates;
+
+  return allInterlinkedIdsOfDashboard;
+}
--- desktop/src/core_plugins/kibana/public/dashboard/lib/update_saved_dashboard.js
+++ desktop/src/core_plugins/kibana/public/dashboard/lib/update_saved_dashboard.js
@@ -3,6 +3,7 @@ import { FilterUtils } from './filter_utils';
 import ConfirmPermissionUpdate from './confirm_permission_update.js';
 import { updateVisualizationPermissions } from './update_visualization_permissions';
 import { updateSavedsearchPermissions } from './update_savedsearch_permissions';
+import { getVisualizationInterlinkedData } from './get_visualizations_interlinked_data';
 
 export async function updateSavedDashboard(
   savedDashboard,
@@ -21,29 +22,39 @@ export async function updateSavedDashboard(
   savedDashboard.uiStateJSON = toJson(uiState.getChanges());
   savedDashboard.optionsJSON = toJson(appState.options);
 
-  const panelData = JSON.parse(savedDashboard.panelsJSON);
   //This visualizationIds is to store dashbaord linked visualization id's
-
   const visualizationIds = [];
+
   //This savedSearchIds is to store dashbaord linked visualization id's
-
   const savedSearchIds = [];
+
   //This is to store the boolean result of any visulization is upgraded to higher permissions or not
-
   let isVisualizationsUpdated = false;
+
   //This is to store the boolean result of any savedSeaches is upgraded to higher permissions or not
-
   let isSavedSearchUpdated = false;
+
   //This is to store the updated visualizations
-
   let updatedVisualizations = [];
-  //This is to store the updated saved searches
 
+  //This is to store the updated saved searches
   let updatedSavedSearches = [];
+
+  //This is to store dashboard user role permissions
+  let dashboardResultRolesData = [];
+
+  //This is to store the updated interlinked visualizations
+  //(EX : Linked BMV's for KPI)
+  let interlinkedUpdatedVisualizations = [];
+
+  //This is to store the inter linked saved searches
+  //(EX : If any visualization is created using a saved search)
+  let interlinkedUpdatedSavedSearches = [];
+
+  let isDashboardPermissionChanged = false;
+  const panelData = JSON.parse(savedDashboard.panelsJSON);
   const dashboardPermissions = savedDashboard.allowedRolesJSON;
   const dashboardUpdatedPermissions = JSON.parse(dashboardPermissions);
-  let isDashboardPermissionChanged = false;
-  let dashboardResultRolesData = [];
 
   //Checking whether the savedDashboard permissions are modified are not with the before modified dashabord data
   await savedDashboards.get(savedDashboard.id).then(async function (dashboardResult) {
@@ -84,10 +95,43 @@ export async function updateSavedDashboard(
       updateSavedsearchPermissions(dashboardPermissions, savedSearchIds, savedSearches)
     );
 
-    await selectedVisualizationsData.then(function (result) {
+    await selectedVisualizationsData.then(async function (result) {
       isVisualizationsUpdated = result.showModal;
       updatedVisualizations = result.visualizations;
+
+      //This interlinkedData stores the id's of interlinked visulizations and saved searches which should also be update
+      const interlinkedData = Promise.resolve(
+        getVisualizationInterlinkedData(updatedVisualizations, dashboardPermissions, savedVisualizations, visualizationIds, savedSearchIds)
+      );
+      await interlinkedData.then(async function (result) {
+        // Getting updated interlinked visualization objects
+        const interLinkedVizsData = Promise.resolve(
+          updateVisualizationPermissions(dashboardPermissions, result.linkedBmvIds, savedVisualizations)
+        );
+        //Getting updated interlinked savedsearch objects
+        const interlinkedSavedSearchesData = Promise.resolve(
+          updateSavedsearchPermissions(dashboardPermissions, result.linkedSavedSearcheIds, savedSearches)
+        );
+        await interLinkedVizsData.then(function (result) {
+          interlinkedUpdatedVisualizations = result.visualizations;
+          //When the directly added visualizations and saved searches are having higer permissions or same permissions
+          //which we added for dashboard that time we need to check for inter linked visualizations and saved searches
+          //to show the confirmation popup for user. Why we are chekcing for not isVisualizationsUpdated is, it may be true
+          //but here it may get false bcz of inter linked data so if higher level permissions are to be upgraded
+          //then no need to check for inter linked visualizations and saved searches without checking only we will see the popup
+          if (!isVisualizationsUpdated) {
+            isVisualizationsUpdated = result.showModal;
+          }
+        });
+        await interlinkedSavedSearchesData.then(function (result) {
+          interlinkedUpdatedSavedSearches = result.savedSearches;
+          if (!isVisualizationsUpdated) {
+            isVisualizationsUpdated = result.showModal;
+          }
+        });
+      });
     });
+
     await selectedSavedSearchesData.then(function (result) {
       isSavedSearchUpdated = result.showModal;
       updatedSavedSearches = result.savedSearches;
@@ -108,14 +152,21 @@ export async function updateSavedDashboard(
       //Based on modal result updating visualizations/savedSearches user role permissions
       await modalInstance.result
         .then(async function () {
-        //Updating visulization/savedSearch user role permissions to the level of dashboard user role permissions
-          await updatedVisualizations.map(async (eachVisualization) => {
+          //Merging Dashboard direct linked and inter linked visulisations updated objects
+          const finalVisualizations = [...updatedVisualizations, ...interlinkedUpdatedVisualizations];
+
+          //Merging Dashboard direct linked and inter linked saved searche updated objects
+          const finalSavedSearches = [...updatedSavedSearches, ...interlinkedUpdatedSavedSearches];
+
+          //Updating visulization/savedSearch user role permissions to the level of dashboard user role permissions
+          await finalVisualizations.map(async (eachVisualization) => {
             await eachVisualization.save();
           });
-          await updatedSavedSearches.map(async (eachSavedSearch) => {
+          await finalSavedSearches.map(async (eachSavedSearch) => {
             await eachSavedSearch.save();
           });
         }, function () {
+
         });
     }
   }
